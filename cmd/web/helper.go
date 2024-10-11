3dea626fb391c22f9a3d8f7847752d8cafd51279
Shashwat1977 <shashwatsachan@gmail.com>
2024-10-12 04:42:09 +0530

    Added the template Cache

---
+++ cmd/web/helper.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func (app *application) render(w http.ResponseWriter, status int, page string, data *templateData) {
+	ts, ok := app.templateCache[page]
+
+	if !ok {
+		err := fmt.Errorf("error occurred while trying to fetch template cache for page %s", page)
+		app.serverError(w, err)
+		return
+	}
+	w.WriteHeader(status)
+	err := ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		err := fmt.Errorf("error occurred while executing template for page %s", page)
+		app.serverError(w, err)
+		return
+	}
+}
+
+func (app *application) newTemplateData() *templateData {
+	return &templateData{
+		CurrentYear: time.Now().Year(),
+	}
+}
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,38 @@
 package main
 
-import "github.com.Shashwat1977.snippetbox/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com.Shashwat1977.snippetbox/internal/models"
+)
 
 type templateData struct {
-	Snippets []*models.Snippet
-	Snippet  *models.Snippet
+	CurrentYear int
+	Snippets    []*models.Snippet
+	Snippet     *models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		files := []string{
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			page,
+		}
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+	return cache, nil
 }
